e9e9673e91480d855e0b1571b41286ce6d10ed4b
lmc-cmd <63010584+lmc-cmd@users.noreply.github.com>
2022-07-05 15:08:37 +0300

    hw 9 (1.2done) 3(inscr

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+`use strict`;
 
+const bodyEl = document.querySelector(`body`);
+const btnStart = document.querySelector(`[data-start]`);
+const btnStop = document.querySelector(`[data-stop]`);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+let repeater = null;
+
+bodyEl.addEventListener(`click`, e => {
+  if (e.target == btnStart) {
+    btnStart.disabled = true;
+    repeater = setInterval(() => {
+      bodyEl.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+  }
+  if (e.target == btnStop) {
+    btnStart.disabled = false;
+    clearInterval(repeater);
+  }
+});
---
+++ src/js/02-timer.js
@@ -0,0 +1,53 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import { convertMs } from './convertMs';
+import Notiflix from 'notiflix';
+const dateInputEl = document.querySelector(`#datetime-picker`);
+const btnStartEl = document.querySelector(`[data-start]`);
+const timerContainerEl = document.querySelector(`.timer`);
+
+const daysEl = timerContainerEl.querySelector(`[data-days]`);
+const hoursEl = timerContainerEl.querySelector(`[data-hours]`);
+const minutesEl = timerContainerEl.querySelector(`[data-minutes]`);
+const secondsEl = timerContainerEl.querySelector(`[data-seconds]`);
+
+btnStartEl.disabled = true;
+let userDate = null;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    if (selectedDates[0] < Date.now()) {
+      Notiflix.Notify.failure('Please choose a date in the future');
+    } else {
+      Notiflix.Notify.success('For countdown start press Start button');
+      btnStartEl.disabled = false;
+      userDate = selectedDates[0];
+    }
+    return userDate;
+  },
+};
+flatpickr(dateInputEl, options);
+
+btnStartEl.addEventListener(`click`, e => {
+  countDown = setInterval(() => {
+    const diff = userDate - Date.now();
+    let convertDiff = convertMs(diff);
+    if (diff < 0) {
+      stop(countDown);
+      return;
+    }
+
+    daysEl.textContent = addLeadingZero(convertDiff.days);
+    hoursEl.textContent = addLeadingZero(convertDiff.hours);
+    minutesEl.textContent = addLeadingZero(convertDiff.minutes);
+    secondsEl.textContent = addLeadingZero(convertDiff.seconds);
+  }, 1000);
+});
+
+function addLeadingZero(value) {
+  return value.toString().padStart(2, '0');
+}
