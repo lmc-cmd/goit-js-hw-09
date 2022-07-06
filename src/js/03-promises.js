1fc10bbbb97da15c5ea9382cb008f8e2bbfe932e
lmc-cmd <63010584+lmc-cmd@users.noreply.github.com>
2022-07-06 21:50:00 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,62 +1,36 @@
 import Notiflix from 'notiflix';
 
+const formEl = document.querySelector('.form');
+formEl.addEventListener(`submit`, e => {
+  e.preventDefault();
+
+  count = 0;
+  startDelay = e.currentTarget.elements.delay.value;
+  stepDelay = e.currentTarget.elements.step.value;
+  delay = stepDelay;
+  // let delay = startDelay;
+  let amount = e.currentTarget.elements.amount.value;
+  setInterval,
+    (intervalId = setInterval(function () {
+      delay = stepDelay * count + +startDelay;
+      count++;
+      if (count == amount) {
+        clearInterval(intervalId);
+      }
+      createPromise(count, delay);
+    }, delay));
+});
+
 function createPromise(position, delay) {
-  delay = 1000;
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
-    Notiflix.Notify.success(`Fullfield `);
+    Notiflix.Notify.success(
+      `Fullfield promise ${position} in ${delay} seconds`
+    );
   } else {
-    Notiflix.Notify.failure(`err`);
+    Notiflix.Notify.failure(`Reject promise ${position} in ${delay} seconds`);
   }
 }
-
-for (let i = createPromise(); i > 5; i + 1) {
-  position = i;
-  delai += delai;
-}
-
-// const promise = new Promise((resolve, reject) => {
-//   setTimeout(() => {
-//     resolve(5);
-//   }, 2000);
-// });
-
-// promise
-//   .then(value => {
-//     console.log(value); // 5
-//     return value * 2;
-//   })
-//   .then(value => {
-//     console.log(value); // 10
-//     return value * 3;
-//   })
-//   .then(value => {
-//     console.log(value); // 30
-//   })
-//   .catch(error => {
-//     console.log(error);
-//   })
-//   .finally(() => {
-//     console.log('Final task');
-//   });
-
-// const fetchUserFromServer = username => {
-//   return new Promise((resolve, reject) => {
-//     console.log(`Fetching data for ${username}`);
-
-//     setTimeout(() => {
-//       // Change value of isSuccess variable to simulate request status
-//       const isSuccess = true;
-
-//       if (isSuccess) {
-//         resolve('success value');
-//       } else {
-//         reject('error');
-//       }
-//     }, 2000);
-//   });
-// };
-
-// fetchUserFromServer('Mango')
-//   .then(user => console.log(user))
-//   .catch(error => console.error(error));
+const onClick = () => {
+  setTimeout(() => {}, startDelay);
+};
