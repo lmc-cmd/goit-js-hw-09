2ea0a2d34f8ad50cf0d9d8f9822185687fcd16ac
lmc-cmd <63010584+lmc-cmd@users.noreply.github.com>
2022-07-07 13:07:21 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -8,29 +8,26 @@ formEl.addEventListener(`submit`, e => {
   startDelay = e.currentTarget.elements.delay.value;
   stepDelay = e.currentTarget.elements.step.value;
   delay = stepDelay;
-  // let delay = startDelay;
   let amount = e.currentTarget.elements.amount.value;
-  setInterval,
-    (intervalId = setInterval(function () {
-      delay = stepDelay * count + +startDelay;
-      count++;
-      if (count == amount) {
-        clearInterval(intervalId);
-      }
-      createPromise(count, delay);
-    }, delay));
+
+  setTimeout(() => {
+    setInterval,
+      (intervalId = setInterval(function () {
+        delay = stepDelay * count + +startDelay;
+        count++;
+        if (count == amount) {
+          clearInterval(intervalId);
+        }
+        createPromise(count, delay);
+      }, delay));
+  }, startDelay);
 });
 
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
-    Notiflix.Notify.success(
-      `Fullfield promise ${position} in ${delay} seconds`
-    );
+    Notiflix.Notify.success(`Fullfield promise ${position} in ${delay} ms`);
   } else {
-    Notiflix.Notify.failure(`Reject promise ${position} in ${delay} seconds`);
+    Notiflix.Notify.failure(`Reject promise ${position} in ${delay} ms`);
   }
 }
-const onClick = () => {
-  setTimeout(() => {}, startDelay);
-};
